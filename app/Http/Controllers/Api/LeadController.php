78b02dfda2a03fe3be36ea33cf7cbb543063be98
srakama <srakama@gmail.com>
2026-05-07 10:48:02 +0200

    Revert "Add cPanel HTTPS mail relay path for lead notifications"
    This reverts commit 954bdec88a56d8f44a1e253ad7c01725e2db8881.

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Mail\LeadCreated;
 use App\Models\Lead;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
@@ -32,27 +31,7 @@ class LeadController extends Controller
             $leadId = $lead->id;
             dispatch(function () use ($notifyTo, $lead, $leadId) {
                 try {
-                    $mailable = new LeadCreated($lead);
-                    $relayUrl = config('services.mail_relay.url');
-                    $relaySecret = config('services.mail_relay.secret');
-
-                    if ($relayUrl && $relaySecret) {
-                        $response = Http::withToken($relaySecret)
-                            ->timeout((int) config('services.mail_relay.timeout', 5))
-                            ->acceptJson()
-                            ->asJson()
-                            ->post($relayUrl, [
-                                'to' => $notifyTo,
-                                'subject' => $mailable->envelope()->subject,
-                                'html' => $mailable->render(),
-                            ]);
-
-                        if (! $response->successful()) {
-                            throw new \RuntimeException('relay returned HTTP ' . $response->status() . ': ' . substr($response->body(), 0, 200));
-                        }
-                    } else {
-                        Mail::to($notifyTo)->send($mailable);
-                    }
+                    Mail::to($notifyTo)->send(new LeadCreated($lead));
                 } catch (\Throwable $e) {
                     Log::warning('Lead notification email failed', [
                         'lead_id' => $leadId,
